15dedc030af11bb717a701a45f1fad4ca6ad0afb
dmitry-izmerov <idd90y@yandex.ru>
2021-01-30 20:27:34 +0100

    Java 6 - Java Compiler API

---
+++ src/main/java/ru/demi/java6/compilerApi/JavaCompilerApiExample.java
@@ -0,0 +1,43 @@
+package ru.demi.java6.compilerApi;
+
+import javax.tools.ToolProvider;
+import java.io.File;
+import java.io.IOException;
+import java.lang.reflect.InvocationTargetException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+public class JavaCompilerApiExample {
+
+    public static void main(String[] args) throws IOException, ClassNotFoundException, NoSuchMethodException,
+            InvocationTargetException, IllegalAccessException {
+
+        var packageName = JavaCompilerApiExample.class.getPackageName();
+        var packagePath = Path.of(packageName.replace('.', File.separatorChar));
+        Path currentDir = Paths.get("src/main/java")
+            .toAbsolutePath()
+            .resolve(packagePath);
+
+        var className = "DynamicCompiled";
+        var code = String.format("""
+                package %s;
+                
+                public class %s {
+                    public static void print() {
+                        System.out.println("Hi from dynamically compiled class");
+                    }
+                }
+            """.stripIndent(), packageName, className);
+
+        var filePath = currentDir.resolve(className + ".java");
+        Files.writeString(filePath, code);
+        var compiledClassesDir = Paths.get("build/classes/java/main").toAbsolutePath().toString();
+        var javac = ToolProvider.getSystemJavaCompiler();
+        javac.run(null, null, null, "-cp", currentDir.toString(), "-d", compiledClassesDir,
+            filePath.toString());
+
+        Class<?> clazz = Class.forName(String.join(".",packageName, className));
+        clazz.getMethod("print").invoke(null);
+    }
+}
