ce9352460dcca02b09b83fdda8a93bfbb29a3540
dmitry-izmerov <idd90y@yandex.ru>
2021-02-06 20:07:14 +0100

    Java 7 - try with resources

---
+++ src/main/java/ru/demi/java7/DiamondOperatorExample.java
@@ -0,0 +1,12 @@
+package ru.demi.java7;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class DiamondOperatorExample {
+
+    public static void main(String[] args) {
+        List<String> listBeforeJava7 = new ArrayList<String>();
+        List<String> listAfterJava7 = new ArrayList<>();
+    }
+}
