fd48fc2f720ae8d354e9d8fc721f56f8ddb6d524
dmitry-izmerov <idd90y@yandex.ru>
2021-02-06 20:13:23 +0100

    Java 7 - underscores in numeric literals

---
+++ src/main/java/ru/demi/java7/UnderscoreInNumbersExample.java
@@ -0,0 +1,10 @@
+package ru.demi.java7;
+
+public class UnderscoreInNumbersExample {
+    public static void main(String[] args) {
+        var number = 1_000_000_000;
+        var doubleNum = 2_000.1_001;
+        System.out.println(number);
+        System.out.println(doubleNum);
+    }
+}
