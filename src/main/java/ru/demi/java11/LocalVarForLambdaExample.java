bed22d8ca8980ef47de93c206efd3e7d9f9334fc
dmitry-izmerov <idd90y@yandex.ru>
2021-02-23 19:08:12 +0100

    Java 11 - Local-Variable Syntax for Lambda Parameters

---
+++ src/main/java/ru/demi/java11/LocalVarForLambdaExample.java
@@ -0,0 +1,22 @@
+package ru.demi.java11;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.List;
+import java.util.Map;
+
+public class LocalVarForLambdaExample {
+    public static final Logger log = LoggerFactory.getLogger(LocalVarForLambdaExample.class);
+
+    public static void main(String[] args) {
+        List.of(1, 2, 3).forEach((var i) -> log.info("value: {}", i));
+
+        Map.ofEntries(
+            Map.entry(1, "one"),
+            Map.entry(2, "two"),
+            Map.entry(3, "three")
+        )
+        .forEach((var key, var value) -> log.info("key: {}, value: {}", key, value));
+    }
+}
